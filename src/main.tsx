9753365553569b3eb43d3bbfc4f7831e63c1a4e8
Fabian Yate <fabianyate31@gmail.com>
2025-03-26 09:38:09 -0500

    Añade el proveedor QueryClient para gestionar consultas con React Query

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { PrimeReactProvider } from "primereact/api";
 
 import "primeicons/primeicons.css";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
@@ -12,10 +13,14 @@ const value = {
   ripple: true,
 };
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <PrimeReactProvider value={value}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </PrimeReactProvider>
   </StrictMode>
 );
