b3cb073dfad55359ae090dd880f9c638352613d6
Fabian Yate <fabianyate31@gmail.com>
2025-03-25 23:34:38 -0500

    Agrega la función getUserInfo para obtener información del usuario y mejora el manejo de la respuesta en registerUser

--- src/services/users/api.ts
+++ src/services/users/api.ts
@@ -1,5 +1,6 @@
 import { axiosConfig } from "../../lib/helpers/axios/axiosConfig";
 import { ApiResponse } from "../../lib/types/ApiResponse";
+import { User } from "../../lib/types/User";
 
 const API_URL = "/users";
 
@@ -8,7 +9,18 @@ export const registerUser = async (data: {
   email: string;
   password: string;
 }): Promise<ApiResponse<void>> => {
-  const response = await axiosConfig.post<ApiResponse<void>>(`${API_URL}/`, data);
+  const response = await axiosConfig.post<ApiResponse<void>>(
+    `${API_URL}/`,
+    data
+  );
+
+  return response.data;
+};
+
+export const getUserInfo = async (
+  userId: number
+): Promise<ApiResponse<User>> => {
+  const response = await axiosConfig.get<ApiResponse<User>>(`${API_URL}/${userId}`);
 
   return response.data;
 };
