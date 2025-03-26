ba63d39625ed0fb619c58aa8f4724f31b40ff0ae
Fabian Yate <fabianyate31@gmail.com>
2025-03-25 23:36:48 -0500

    Refactor PrivateRoute para usar el estado de autenticación desde el store y mejorar la gestión de la carga de sesión

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,21 +1,23 @@
 import { Navigate, useLocation } from "react-router";
-import { toast } from "sonner";
-import { useAuthContext } from "../lib/hooks/useAuth";
+import { ROUTES } from "../lib/constants";
+import { useAuthStore } from "../lib/store/useAuthStore";
 
 type PrivateRouteProps = {
   children: React.ReactNode;
 };
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
-  const { token, isLoading } = useAuthContext();
   const location = useLocation();
 
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+  const isLoading = useAuthStore((state) => state.isLoading);
+
   if (isLoading) {
-    return toast.loading("Validando token...");
+    return <div className="p-4 text-center">Validando sesión...</div>;
   }
 
-  if (!token) {
-    return <Navigate to="/login" state={{ from: location }} replace />;
+  if (!isLoggedIn) {
+    return <Navigate to={ROUTES.LOGIN} state={{ from: location }} replace />;
   }
 
   return <>{children}</>;
