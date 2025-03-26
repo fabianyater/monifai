e04e2e13d1e63b7238c0742d80fec5d3605bda20
Fabian Yate <fabianyate31@gmail.com>
2025-03-26 06:17:33 -0500

    Añade verificación de sesión al cargar la aplicación y cambia la posición del Toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { Toaster } from "sonner";
 import RoutesPage from "./routes/routes";
+import { useEffect } from "react";
+import { useAuthStore } from "./lib/store/useAuthStore";
 
 function App() {
+
+  const checkSession = useAuthStore((state) => state.checkSession);
+
+  useEffect(() => {
+    checkSession();
+  }, [checkSession]);
   return (
     <>
       <RoutesPage></RoutesPage>
-      <Toaster position="top-right" />
+      <Toaster position="bottom-left" />
     </>
   );
 }
