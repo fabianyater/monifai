8d09b9576e5d89f7d20f2e6044ce622aa281bc16
Fabian Yate <fabianyate31@gmail.com>
2025-03-26 09:37:50 -0500

    Refactor la clave de consulta en useUser para utilizar userKeys

--- src/services/users/queries.ts
+++ src/services/users/queries.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { getUserInfo } from "./api";
+import { userKeys } from "./keys";
 
 export const useUser = (id: number) => {
   return useQuery({
-    queryKey: ["user", id],
+    queryKey: [userKeys.user(id)],
     queryFn: async () => {
       const response = await getUserInfo(id);
       return response.data;
