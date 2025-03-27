4a321493eb58c0199bfe3f0e1a234dd74e8bc322
Fabian Yate <fabianyate31@gmail.com>
2025-03-26 20:15:09 -0500

    Añade selección de tipo de transacción en la página de inicio y mejora el componente MaiSelect para gestionar valores seleccionados

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,10 +4,15 @@ import { useEffect, useState } from "react";
 import { MaiSelect } from "../components/atoms/MaiSelect";
 import { useUserStore } from "../lib/store/useUserStore";
 import { Pocket } from "../lib/types/Pocket";
+import { TransactionType, transactionTypes } from "../lib/types/Transactions";
 import { usePockets } from "../services/pockets/queries";
 
 export const HomePage = () => {
   const [selectedPocket, setSelectedPocket] = useState<Pocket | null>(null);
+  const [transactionType, setTransactionType] = useState<TransactionType>(
+    transactionTypes[1]
+  );
+
   const userId = useUserStore((state) => state.userId);
   const user = useUserStore((state) => state.user);
   const { queryKey, queryFn } = usePockets(Number(userId));
@@ -33,19 +38,42 @@ export const HomePage = () => {
         )}
         !
       </h1>
-      <section className="w-2/5 mt-4">
-        <div className="flex flex-col gap-2 items-start">
+      <section className="w-full mt-4 flex">
+        <div className="w-2/5 flex flex-col gap-2 items-start">
           <div className="flex items-center justify-start">
             <span>Este mes en </span>
-            <MaiSelect
+            <MaiSelect<Pocket>
               data={data || []}
-              selectedPocket={selectedPocket}
-              setSelectedPocket={setSelectedPocket}
+              selectedValue={selectedPocket}
+              setSelectedValue={(value: Pocket) => setSelectedPocket(value)}
               isLoading={isLoading}
+              optionLabel="name"
+              optionValue="id"
             />
           </div>
         </div>
-        <div className="flex flex-col gap-2 items-center justify-start"></div>
+        <div className="w-full flex flex-col gap-2 items-center justify-start">
+          <div className="flex items-center justify-center w-full bg-gray-600 rounded-3xl">
+            <div className="flex flex-col">
+              <MaiSelect<TransactionType>
+                data={transactionTypes}
+                selectedValue={transactionType}
+                setSelectedValue={setTransactionType}
+                optionLabel="label"
+                optionValue="value"
+              />
+              <div className="flex items-center justify-center gap-2">
+                <span className="text-center font-medium">Total </span>
+                <span
+                  className="
+                    font-black"
+                >
+                  $-50.90
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
