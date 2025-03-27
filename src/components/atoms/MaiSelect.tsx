c0099cc06915aa8a961817f1699efa1a0aa509c0
Fabian Yate <fabianyate31@gmail.com>
2025-03-26 20:15:05 -0500

    Genera un componente MaiSelect genérico para seleccionar valores, permitiendo personalización de etiquetas y valores, y mejora la gestión de opciones seleccionadas.

--- src/components/atoms/MaiSelect.tsx
+++ src/components/atoms/MaiSelect.tsx
@@ -1,40 +1,79 @@
 import { Dropdown } from "primereact/dropdown";
-import { Pocket } from "../../lib/types/Pocket";
 
-type MaiSelectProps = {
-  data: Pocket[];
-  selectedPocket: Pocket | null;
-  setSelectedPocket: (pocket: Pocket) => void;
-  isLoading: boolean;
+type MaiSelectProps<T> = {
+  data: T[];
+  selectedValue: T | null;
+  setSelectedValue: (value: T) => void;
+  isLoading?: boolean;
+  optionLabel?: string;
+  optionValue?: keyof T;
+  placeholder?: string;
 };
 
-export const MaiSelect = ({
+const ValueTemplate = <T,>({
+  option,
+  props,
+  optionLabel,
+}: {
+  option: T | null;
+  props: { placeholder: string };
+  optionLabel: keyof T;
+}) => {
+  if (!option) {
+    return <span className="text-gray-400">{props.placeholder}</span>;
+  }
+
+  const label = option[optionLabel];
+  const isTransactionType =
+    option &&
+    typeof option === "object" &&
+    "value" in option &&
+    (option["value"] === "expenses" || option["value"] === "incomes");
+
+  const labelClass = isTransactionType ? "text-xl font-semibold" : "";
+
+  return (
+    <div className="flex items-center gap-2 text-gray">
+      {option && typeof option === "object" && "emoji" in option && (
+        <span>{(option as { emoji: string }).emoji}</span>
+      )}
+      <span className={`${labelClass}`}>{label as React.ReactNode}</span>
+    </div>
+  );
+};
+
+export const MaiSelect = <T,>({
   data,
-  selectedPocket,
-  setSelectedPocket,
+  selectedValue,
+  setSelectedValue,
   isLoading,
-}: MaiSelectProps) => {
+  optionLabel = "name",
+  optionValue = "value" as keyof T,
+  placeholder,
+}: MaiSelectProps<T>) => {
   return (
     <Dropdown
       className="bg-transparent outline-none border-none focus:ring-0 shadow-none"
       options={data}
-      optionLabel="name"
-      placeholder="Selecciona tu bolsillo"
-      onChange={(e) => setSelectedPocket(e.value)}
-      value={selectedPocket}
-      loading={isLoading}
-      valueTemplate={(option, props) => {
-        if (!option) {
-          return <span className="text-gray-400">{props.placeholder}</span>;
-        }
-
-        return (
-          <div className="flex items-center gap-2 text-white">
-            <span>{option.emoji}</span>
-            <span>{option.name}</span>
-          </div>
-        );
+      optionLabel={optionLabel}
+      optionValue={optionValue as string}
+      placeholder={placeholder ?? "Selecciona una opción"}
+      onChange={(e) => {
+        const selected = data.find((item) => item[optionValue] === e.value);
+        setSelectedValue(selected as T);
       }}
+      value={selectedValue ? selectedValue[optionValue] : null}
+      loading={isLoading}
+      valueTemplate={(option, props) => (
+        <ValueTemplate
+          option={option}
+          props={{
+            ...props,
+            placeholder: props.placeholder ?? "Selecciona una opción",
+          }}
+          optionLabel={optionLabel as keyof T}
+        />
+      )}
       pt={{
         root: {
           className: "bg-transparent border-none outline-none shadow-none",
