4e0f3743acd60ecb6f09afa8f68f2216ceb9d780
Fabian Yate <fabianyate31@gmail.com>
2025-04-03 09:20:45 -0500

    Añade funcionalidad para gestionar categorías con servicios para obtener y crear categorías, junto con claves y consultas asociadas

---
+++ src/services/categories/keys.ts
@@ -0,0 +1,3 @@
+export const categoriesKeys = {
+  categories: ["categories"] as const,
+};
---
+++ src/services/categories/mutations.ts
@@ -0,0 +1,18 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createCategory } from "./api";
+import { categoriesKeys } from "./keys";
+
+export const useCreateCategory = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationKey: [...categoriesKeys.categories, "create"],
+    mutationFn: createCategory,
+    onSuccess: () => {
+      console.log("✅ Categoría creada");
+      queryClient.invalidateQueries({
+        queryKey: categoriesKeys.categories,
+        refetchType: "active",
+      });
+    },
+  });
+};
