d6f97ce7703bd1645b3a2ee08e1aef53bd651d19
Fabian Yate <fabianyate31@gmail.com>
2025-04-03 09:21:22 -0500

    Refactor tipos de transacciones: redefine TransactionType y añade la interfaz Transaction con propiedades detalladas

--- src/lib/types/Transactions.ts
+++ src/lib/types/Transactions.ts
@@ -1,15 +1,30 @@
-export type TransactionType = {
+export type TransactionType = 'INCOME' | 'EXPENSE';
+
+export type TransactionTypeOption = {
   label: string;
-  value: "incomes" | "expenses";
+  value: TransactionType;
 };
 
-export const transactionTypes: TransactionType[] = [
+export interface Transaction {
+  id: number;
+  uuid: string;
+  description: string;
+  amount: number;
+  date: Date;
+  periodicity: 'DAILY' | 'WEEKLY' | 'MONTHLY' | 'YEARLY';
+  transactionType: TransactionType;
+  categoryId: number;
+  pocketId: number;
+}
+
+
+export const transactionTypes: TransactionTypeOption[] = [
   {
     label: "Ingresos",
-    value: "incomes",
+    value: "INCOME",
   },
   {
     label: "Gastos",
-    value: "expenses",
+    value: "EXPENSE",
   },
 ];
