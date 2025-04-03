5ffe45580e8ab4775ee2d1a79bac5c2155c6571e
Fabian Yate <fabianyate31@gmail.com>
2025-04-03 09:21:44 -0500

    Añade gestión de bolsillo seleccionado: incluye propiedades y métodos para manejar el bolsillo seleccionado en el store

--- src/lib/store/usePocketStore.tsx
+++ src/lib/store/usePocketStore.tsx
@@ -4,11 +4,15 @@ import { Pocket } from "../types/Pocket";
 type PocketStore = {
   pockets: Pocket[];
   setPockets: (pockets: Pocket[]) => void;
+  selectedPocket: Pocket | null;
+  setSelectedPocket: (pocket: Pocket | null) => void;
   isLoading: boolean;
 };
 
 export const usePocketStore = create<PocketStore>((set) => ({
   pockets: [],
   setPockets: (pockets: Pocket[]) => set({ pockets }),
+  selectedPocket: null,
+  setSelectedPocket: (pocket: Pocket | null) => set({ selectedPocket: pocket }),
   isLoading: true,
 }));
