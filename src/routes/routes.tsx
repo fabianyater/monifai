dc3e32a42fbb472dc3b10c37ac292fde0cdf4371
Fabian Yate <fabianyate31@gmail.com>
2025-04-03 09:21:13 -0500

    Añade la página de categorías a las rutas de la aplicación

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import { HomeLayout } from "../components/templates/HomeLayout";
 import { ROUTES } from "../lib/constants";
+import { CategoriesPage } from "../pages/CategoriesPage";
 import { HomePage } from "../pages/HomePage";
 import { LoginPage } from "../pages/LoginPage";
 import { NotFoundPage } from "../pages/NotFoundPage";
@@ -8,28 +9,26 @@ import { RegisterPage } from "../pages/RegisterPage";
 import PrivateRoute from "./PrivateRoute";
 
 export default function RoutesPage() {
- 
-  
   return (
     <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <PrivateRoute>
-                <HomeLayout />
-              </PrivateRoute>
-            }
-          >
-            <Route index element={<Navigate to={ROUTES.HOME} replace />} />
-            <Route path={ROUTES.HOME} element={<HomePage />} />
-            <Route path={ROUTES.CATEGORIES} element={<div>Categorías</div>} />
-          </Route>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <HomeLayout />
+            </PrivateRoute>
+          }
+        >
+          <Route index element={<Navigate to={ROUTES.HOME} replace />} />
+          <Route path={ROUTES.HOME} element={<HomePage />} />
+          <Route path={ROUTES.CATEGORIES} element={<CategoriesPage />} />
+        </Route>
 
-          <Route path={ROUTES.LOGIN} element={<LoginPage />} />
-          <Route path={ROUTES.REGISTER} element={<RegisterPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+        <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+        <Route path={ROUTES.REGISTER} element={<RegisterPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
