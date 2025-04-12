5845263d23cb025f0a865a1344ac4268aa3a8a73
Fabian Yate <fabianyate31@gmail.com>
2025-04-11 22:57:35 -0500

    Añade la función useTransactionSummaryByCategory para obtener el resumen de transacciones por categoría

--- src/services/transactions/keys.ts
+++ src/services/transactions/keys.ts
@@ -2,4 +2,5 @@ export const transactionKeys = {
   monthlyBalanceByType: ["monthlyBalanceByType"] as const,
   classifyTransaction: ["classifyTransaction"] as const,
   createTransaction: ["createTransaction"] as const,
+  transactionSummaryByCategory: ["transactionSummaryByCategory"] as const,
 };
--- src/services/transactions/queries.ts
+++ src/services/transactions/queries.ts
@@ -1,4 +1,8 @@
-import { getMonthlyExpenses, getMonthlyIncomes } from "./api";
+import {
+  getMonthlyExpenses,
+  getMonthlyIncomes,
+  getTransactionsSummaryByCategory,
+} from "./api";
 import { transactionKeys } from "./keys";
 
 export const useMonthlyBalance = (pocketId: number, txnType: string) => {
@@ -9,6 +13,20 @@ export const useMonthlyBalance = (pocketId: number, txnType: string) => {
         txnType === "INCOME"
           ? getMonthlyIncomes(pocketId)
           : getMonthlyExpenses(pocketId);
+      return response;
+    },
+    enabled: !!pocketId && !!txnType,
+  };
+};
+
+export const useTransactionSummaryByCategory = (
+  pocketId: number,
+  txnType: string
+) => {
+  return {
+    queryKey: [transactionKeys.transactionSummaryByCategory, pocketId, txnType],
+    queryFn: async () => {
+      const response = getTransactionsSummaryByCategory(pocketId, txnType);
       return response;
     },
     enabled: !!pocketId && !!txnType,
