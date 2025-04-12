7607bb625aeac1c39b4394689b2b071a0e4d953f
Fabian Yate <fabianyate31@gmail.com>
2025-04-11 22:57:48 -0500

    Añade el componente CategoryChart para visualizar el resumen de transacciones por categoría

---
+++ src/components/organisms/CategoryChart.tsx
@@ -0,0 +1,67 @@
+import { useQuery } from "@tanstack/react-query";
+import { useTransactionStore } from "../../lib/store/useTransactionStore";
+import { useTransactionSummaryByCategory } from "../../services/transactions/queries";
+import { Spinner } from "../atoms/Spinner";
+
+type CategoryChartProps = {
+  pocketId: number;
+};
+
+import { motion } from "framer-motion";
+import { Tooltip } from "primereact/tooltip";
+
+export const CategoryChart = ({ pocketId }: CategoryChartProps) => {
+  const transactionType = useTransactionStore((state) => state.transactionType);
+  const { queryKey, queryFn, enabled } = useTransactionSummaryByCategory(
+    pocketId,
+    transactionType
+  );
+
+  const { data, isLoading } = useQuery({
+    queryKey,
+    queryFn,
+    enabled,
+  });
+
+  if (isLoading) return <Spinner />;
+
+  const maxAmount = Math.max(...(data?.map((c) => c.totalAmount) ?? [1]));
+
+  return (
+    <div className="flex flex-col items-center justify-end w-full bg-[#2D2D2D] rounded-3xl py-4 h-[300px]">
+      <div
+        className="flex justify-start items-end gap-2 w-full px-6"
+        style={{ height: 250 }}
+      >
+        <Tooltip target=".custom-tooltip-div" />
+        {data?.map((category) => {
+          const heightPx = (category.totalAmount / maxAmount) * 250;
+
+          return (
+            <div
+              key={category.id}
+              className="flex flex-col items-center w-14 cursor-pointer custom-tooltip-div"
+              data-pr-tooltip={category.name}
+            >
+              <motion.div
+                className="w-full bg-black/40 rounded-xl transition-all duration-500 flex flex-col items-center justify-end text-white text-sm hover:bg-black"
+                initial={{ height: 0 }}
+                animate={{ height: Math.max(heightPx, 48) }}
+                transition={{ duration: 0.5, ease: "easeOut" }}
+              >
+                <span className="text-xl leading-none">
+                  {category.emoji[0]}
+                </span>
+                <span className="text-xs leading-none mb-1">
+                  {category.totalAmount === 0
+                    ? "0"
+                    : `${category.totalAmount / 1000}K`}
+                </span>
+              </motion.div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
