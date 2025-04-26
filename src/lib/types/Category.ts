8ef24833b69a3d007689d8303f20e3e73d02ad2a
Fabian Yate <fabianyate31@gmail.com>
2025-04-26 16:48:43 -0500

    Añade el tipo UpdateCategoryDefaultEmojiRequest para actualizar el emoji por defecto de una categoría.

--- src/lib/types/Category.ts
+++ src/lib/types/Category.ts
@@ -2,8 +2,14 @@ export type Category = {
   id: number;
   name: string;
   emojis: string[];
+  defaultEmoji?: string;
 };
 
 export type CategoryRequest = Omit<Category, "emojis" | "id"> & {
   id?: number;
+};
+
+export type UpdateCategoryDefaultEmojiRequest = {
+  categoryName: string;
+  newEmoji: string;
 };
