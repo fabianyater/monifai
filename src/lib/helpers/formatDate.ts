7b106cfe0f3d80312d0de74cc100b1b51b88f1f3
Fabian Yate <fabianyate31@gmail.com>
2025-04-26 21:42:28 -0500

    Añade la función formatDate para formatear fechas en el formato "día de mes de año" en español.

---
+++ src/lib/helpers/formatDate.ts
@@ -0,0 +1,10 @@
+export const formatDate = (date: string | Date) => {
+  const dateObj = new Date(date);
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  return new Intl.DateTimeFormat("es-CO", options).format(dateObj);
+};
