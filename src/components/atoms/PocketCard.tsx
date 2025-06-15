5d5c966bfd832ebbed2fa28d00cf3718f3515ba0
Fabian Yate <fabianyate31@gmail.com>
2025-06-15 01:50:13 -0500

    agregar prop para abrir modal y guardar la billetara a guardar

--- src/components/atoms/PocketCard.tsx
+++ src/components/atoms/PocketCard.tsx
@@ -1,14 +1,21 @@
 import { Ripple } from "primereact/ripple";
+import { usePocketStore } from "../../lib/store/usePocketStore";
 import { Pocket } from "../../lib/types/Pocket";
 
 type PocketCardProps = {
   pocket: Pocket;
+  setIsDialogOpen: (isDialogOpen: boolean) => void;
 };
 
-export const PocketCard = ({ pocket }: PocketCardProps) => {
+export const PocketCard = ({ pocket, setIsDialogOpen }: PocketCardProps) => {
+  const pocketToEdit = usePocketStore((state) => state.setPocketToEdit);
   return (
     <div
       className="flex flex-col items-center gap-3 group cursor-pointe cursor-pointer"
+      onClick={() => {
+        setIsDialogOpen(true);
+        pocketToEdit(pocket);
+      }}
     >
       <div className="w-24 h-24 rounded-xl bg-neutral-800 hover:bg-neutral-700 transition-colors duration-300 relative overflow-hidden flex items-center justify-center shadow-md">
         <Ripple />
