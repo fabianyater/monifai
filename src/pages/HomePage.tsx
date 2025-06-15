2bc928c98a6a0e8edc29383002814c0dbd0b5a4f
Fabian Yate <fabianyate31@gmail.com>
2025-06-14 23:50:46 -0500

    refactor general del Home Page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,13 @@
-import { useQuery } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
-import { Greet } from "../components/atoms/Greet";
+import { useState } from "react";
 import { MaiButton } from "../components/atoms/MaiButton";
-import { MaiSelect } from "../components/atoms/MaiSelect";
+import { VoiceInput } from "../components/atoms/VoiceInput";
 import { CreateTransactionModal } from "../components/molecules/CreateTransactionModal";
 import { CreateTransferModal } from "../components/molecules/CreateTransferModal";
+import { ExpenseSummary } from "../components/molecules/ExpenseSummary";
 import { CategoryChart } from "../components/organisms/CategoryChart";
-import { TransactionCard } from "../components/organisms/TransactionCard";
+import RecentTransactions from "../components/organisms/RecentTransactions";
 import { usePocketStore } from "../lib/store/usePocketStore";
 import { useTransactionStore } from "../lib/store/useTransactionStore";
-import { useUserStore } from "../lib/store/useUserStore";
-import { Pocket } from "../lib/types/Pocket";
-import { usePockets } from "../services/pockets/queries";
 
 export const HomePage = () => {
   const [isTransferModalOpen, setIsTransferModalOpen] =
@@ -22,62 +18,19 @@ export const HomePage = () => {
   const setisTransactionModalOpen = useTransactionStore(
     (state) => state.setIsTransactionModalOpen
   );
-  const user = useUserStore((state) => state.user);
   const selectedPocket = usePocketStore((state) => state.selectedPocket);
-  const setSelectedPocket = usePocketStore((state) => state.setSelectedPocket);
-  const { queryKey, queryFn } = usePockets();
-  const { data, isLoading } = useQuery({
-    queryKey,
-    queryFn,
-  });
-
-  useEffect(() => {
-    if (data && data.length > 0 && !selectedPocket) {
-      setSelectedPocket(data[0]);
-    }
-  }, [data, selectedPocket, setSelectedPocket]);
-
-  const openModal = () => {
-    setisTransactionModalOpen(true);
-  };
 
   return (
-    <div className="">
-      <section className="w-full flex flex-col  gap-4 md:flex-row">
-        <div className="w-full md:w-2/5 flex flex-col gap-2 items-start">
-          <Greet name={user?.name} />
-          <div className="w-max">
-            <span>Este mes en </span>
-            <MaiSelect<Pocket>
-              data={data || []}
-              selectedValue={selectedPocket}
-              setSelectedValue={(value: Pocket) => setSelectedPocket(value)}
-              isLoading={isLoading}
-              optionLabel="name"
-              optionValue="id"
-            />
-          </div>
-          <div className="w-full flex gap-2 items-center justify-start">
-            <MaiButton
-              icon="pi pi-plus"
-              label="Añadir transaccción"
-              className="w-full bg-transparent text-white border-dashed border-2 border-gray-500 hover:bg-primary rounded-2xl"
-              type="button"
-              onClick={openModal}
-            />
-            <MaiButton
-              icon="pi pi-arrow-right-arrow-left"
-              type="button"
-              className="bg-transparent text-white border-dashed border-2 border-gray-500 hover:bg-primary rounded-xl"
-              onClick={() => setIsTransferModalOpen(true)}
-            />
-          </div>
-        </div>
-        <div className="w-full flex flex-col gap-2 items-center justify-start">
-          <TransactionCard />
-          {selectedPocket && <CategoryChart pocketId={selectedPocket.id} />}
-        </div>
-      </section>
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+      <div className="lg:col-span-3">
+        <ExpenseSummary />
+      </div>
+      <div className="lg:col-span-2">
+        {selectedPocket && <CategoryChart pocketId={selectedPocket.id} />}
+      </div>
+      <div className="lg:col-span-1">
+        <RecentTransactions pocketId={selectedPocket?.id ?? 0} />
+      </div>
       {isTransactionModalOpen && (
         <CreateTransactionModal
           onClose={() => {
@@ -91,6 +44,23 @@ export const HomePage = () => {
           setIsDialogOpen={setIsTransferModalOpen}
         />
       )}
+      <div className="fixed bottom-0 left-0 right-0 flex justify-center items-end flex-col gap-4 p-4">
+        <MaiButton
+          icon="pi pi-plus text-white text-sm"
+          className=" bg-purple-600 shadow-sm p-0 group border-none"
+          type="button"
+          rounded
+          onClick={() => setisTransactionModalOpen(true)}
+        />
+        <MaiButton
+          icon="pi pi-arrow-right-arrow-left text-white text-sm"
+          className="bg-purple-600 shadow-sm p-0 group border-none"
+          type="button"
+          rounded
+          onClick={() => setIsTransferModalOpen(true)}
+        />
+        <VoiceInput />
+      </div>
     </div>
   );
 };
