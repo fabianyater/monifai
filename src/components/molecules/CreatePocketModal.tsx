5ad48ea7e293fda69807833977d84bde0e52ebf8
Fabian Yate <fabianyate31@gmail.com>
2025-06-15 01:49:40 -0500

    actuaalizar formulario para poder editar y crear bolsillos

--- src/components/molecules/CreatePocketModal.tsx
+++ src/components/molecules/CreatePocketModal.tsx
@@ -1,53 +1,99 @@
 import { Dialog } from "primereact/dialog";
 import { InputSwitch } from "primereact/inputswitch";
-import { useState } from "react";
-import { PocketRequest } from "../../lib/types/Pocket";
-import { useCreatePocket } from "../../services/pockets/mutations";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import { PocketRequest, PocketResponse } from "../../lib/types/Pocket";
+import {
+  useCreatePocket,
+  useUpdatePocket,
+} from "../../services/pockets/mutations";
 import { Input } from "../atoms/Input";
 import { MaiButton } from "../atoms/MaiButton";
 
 type CreatePocketModalProps = {
   isDialogOpen: boolean;
   setIsDialogOpen: (isDialogOpen: boolean) => void;
+  pocketToEdit?: PocketResponse;
 };
 
 export const CreatePocketModal = ({
   isDialogOpen,
   setIsDialogOpen,
+  pocketToEdit,
 }: CreatePocketModalProps) => {
   const [formData, setFormData] = useState<PocketRequest>({
     name: "",
     balance: 0,
     excludeBalance: false,
+    emoji: "",
   });
 
-  const { mutate, isPending } = useCreatePocket();
+  const { mutate: createPocket, isPending: isCreating } = useCreatePocket();
+  const { mutate: updatePocket, isPending: isUpdating } = useUpdatePocket();
+
+  useEffect(() => {
+    if (pocketToEdit) {
+      setFormData({
+        name: pocketToEdit.name,
+        balance: pocketToEdit.balance,
+        excludeBalance: pocketToEdit.excludeBalance,
+        emoji: pocketToEdit.emoji,
+      });
+    } else {
+      setFormData({ name: "", balance: 0, excludeBalance: false, emoji: "" });
+    }
+  }, [pocketToEdit]);
 
   const handleCreate = async (e: React.FormEvent) => {
     e.preventDefault();
     if (!formData.name.trim()) return;
 
-    mutate(
-      {
-        name: formData.name.trim(),
-        balance: formData.balance,
-        excludeBalance: formData.excludeBalance,
-      },
-      {
-        onSuccess: () => {
-          setIsDialogOpen(false);
-          setFormData({ name: "", balance: 0, excludeBalance: false });
+    if (pocketToEdit) {
+      updatePocket(
+        {
+          pocketId: pocketToEdit.id,
+          pocket: { ...formData },
         },
-        onError: (error) => {
-          console.error("Error al crear bolsillo:", error);
+        {
+          onSuccess: () => {
+            setIsDialogOpen(false);
+            setFormData({ name: "", balance: 0, excludeBalance: false });
+            toast.success("Bolsillo actualizado correctamente", {
+              icon: "🎉",
+            });
+          },
+          onError: (error) => {
+            console.error("Error al actuaalizar el bolsillo:", error);
+          },
+        }
+      );
+    } else {
+      createPocket(
+        {
+          name: formData.name.trim(),
+          balance: formData.balance,
+          excludeBalance: formData.excludeBalance,
         },
-      }
-    );
+        {
+          onSuccess: () => {
+            setIsDialogOpen(false);
+            setFormData({ name: "", balance: 0, excludeBalance: false });
+          },
+          onError: (error) => {
+            console.error("Error al crear bolsillo:", error);
+          },
+        }
+      );
+    }
   };
 
   return (
     <Dialog
-      header={<span className="text-xl font-medium">Nuevo bolsillo</span>}
+      header={
+        <span className="text-xl font-medium">
+          {pocketToEdit ? "Editar bolsillo" : "Nuevo bolsillo"}
+        </span>
+      }
       visible={isDialogOpen}
       onHide={() => setIsDialogOpen(false)}
       dismissableMask
@@ -77,19 +123,35 @@ export const CreatePocketModal = ({
             setFormData({ ...formData, balance: Number(e.target.value) })
           }
         />
-        <label className="flex items-center justify-between text-gray-400 text-sm">
-          Excluir saldo del balance general
-          <InputSwitch
-            checked={formData.excludeBalance}
+        <div className="flex flex-col items-start justify-between">
+          <p>Excluir del saldo</p>
+          <label className="w-full flex items-center justify-between text-sm text-gray-400">
+            Ignora el saldo de este bolsillo en modo total
+            <InputSwitch
+              className="scale-75"
+              checked={formData.excludeBalance}
+              onChange={(e) =>
+                setFormData({ ...formData, excludeBalance: e.target.value })
+              }
+            />
+          </label>
+        </div>
+        {pocketToEdit && (
+          <Input
+            label="Emoji"
+            type="text"
+            placeholder="🍔"
+            name="emoji"
+            value={formData.emoji}
             onChange={(e) =>
-              setFormData({ ...formData, excludeBalance: e.target.value })
+              setFormData({ ...formData, emoji: e.target.value })
             }
           />
-        </label>
+        )}
         <MaiButton
-          label="Crear"
+          label={!pocketToEdit ? "Crear" : "Editar"}
           type="submit"
-          loading={isPending}
+          loading={isCreating || isUpdating}
           disabled={!formData.name.trim()}
           className="w-full"
         />
