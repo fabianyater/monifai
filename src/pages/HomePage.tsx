4cc3250e1e483462f15d10d20faa632773d3123b
Fabian Yate <fabianyate31@gmail.com>
2025-06-15 21:35:52 -0500

    agrega control de botones para navegar entre fecha y ver los valores respectivos según la fecha seleccionada

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,5 @@
+import { Button } from "primereact/button";
+import { Calendar } from "primereact/calendar";
 import { useState } from "react";
 import { MaiButton } from "../components/atoms/MaiButton";
 import { VoiceInput } from "../components/atoms/VoiceInput";
@@ -6,10 +8,13 @@ import { CreateTransferModal } from "../components/molecules/CreateTransferModal
 import { ExpenseSummary } from "../components/molecules/ExpenseSummary";
 import { CategoryChart } from "../components/organisms/CategoryChart";
 import RecentTransactions from "../components/organisms/RecentTransactions";
+import { useDateSelectorStore } from "../lib/store/useDateSelectorStore";
 import { usePocketStore } from "../lib/store/usePocketStore";
 import { useTransactionStore } from "../lib/store/useTransactionStore";
 
 export const HomePage = () => {
+  const date = useDateSelectorStore((state) => state.date);
+  const setDate = useDateSelectorStore((state) => state.setDate);
   const [isTransferModalOpen, setIsTransferModalOpen] =
     useState<boolean>(false);
   const isTransactionModalOpen = useTransactionStore(
@@ -20,10 +25,47 @@ export const HomePage = () => {
   );
   const selectedPocket = usePocketStore((state) => state.selectedPocket);
 
+  const handleNextMonth = () => {
+    const newDate = new Date(date);
+    newDate.setMonth(newDate.getMonth() + 1);
+    setDate(newDate);
+  };
+
+  const handlePreviousMonth = () => {
+    const newDate = new Date(date);
+    newDate.setMonth(newDate.getMonth() - 1);
+    setDate(newDate);
+  };
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
       <div className="lg:col-span-3">
-        <ExpenseSummary />
+        <ExpenseSummary/>
+      </div>
+      <div className="lg:col-span-2">
+        <div className="flex items-center justify-between">
+          <Button
+            icon="pi pi-arrow-left"
+            size="small"
+            onClick={handlePreviousMonth}
+          />
+          <Calendar
+            value={date}
+            onChange={(e) => {
+              if (e.target.value) {
+                setDate(e.target.value);
+              }
+            }}
+            view="month"
+            dateFormat="MM"
+            showButtonBar
+          />
+          <Button
+            icon="pi pi-arrow-right"
+            size="small"
+            onClick={handleNextMonth}
+          />
+        </div>
       </div>
       <div className="lg:col-span-2">
         {selectedPocket && <CategoryChart pocketId={selectedPocket.id} />}
